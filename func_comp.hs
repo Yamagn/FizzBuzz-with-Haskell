c95ed06aa705f468040b823155cfb7d1503bd739
Yamagn <syankenpon@gmail.com>
2018-10-31 22:49:48 +0900

    add func_comp

--- func_comp.hs
+++ func_comp.hs
@@ -4,6 +4,12 @@ main = do
     print $ map (negate . sum . tail) [[1..5], [3..6], [1..7]]
     print $ sum . replicate 5 $ max 6.7 8.9
     print $ replicate 2 . product . map (*3) $ zipWith max [1,2] [4,5]
+    print $ oddSquareSum
 
 sum' :: (Num a) => [a] -> a
-sum' xs = foldl (+) 0 xs
+sum' = foldl (+) 0
+
+fn = ceiling . negate . tan . cos . max 50
+
+oddSquareSum :: Integer
+oddSquareSum = sum . takeWhile (<100) . filter odd $ map (^2) [1..]
